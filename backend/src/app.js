fe8df9ab4a5ca81a528e203b226af7e6b165741c
Italo M <italomarcos0010@gmail.com>
2020-03-15 17:43:33 -0300

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,22 @@ class App {
   }
 
   middlewares() {
-    this.server.use(cors({ origin: "https://objective-einstein-06fb51.netlify.com" }));
+    const allowedOrigins = ['https://objective-einstein-06fb51.netlify.com/'];
+
+    this.server.use(
+      cors({
+        origin(origin, callback) {
+          if (!origin) return callback(null, true);
+          if (allowedOrigins.indexOf(origin) === -1) {
+            const msg =
+              'The CORS policy for this site does not ' +
+              'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+          }
+          return callback(null, true);
+        },
+      })
+    );
     this.server.use(express.json());
   }
 
